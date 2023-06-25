00b4c757fe728a7bf4191b2a5b2b248ce3628fcf
Eloah Baracho <eloahbarachodossantos@gmail.com>
2023-06-25 12:51:00 -0300

    Add files via upload

---
+++ swift01.playground/Contents.swift
@@ -0,0 +1,7 @@
+var inicialNome: Character = "E"
+var nome: String = "Eloah"
+var idade: Int = 20
+var apelido: String = "Elo"
+var altura: Float = 1.67
+var gostaDeJava: Bool = true
+print("Sua inicial: \(inicialNome), Seu nome: \(nome), Sua idade: \(idade), Sua Altura: \(altura), Seu gosto por java: \(gostaDeJava)")
