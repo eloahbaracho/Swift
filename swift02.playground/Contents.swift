eaa0b077ed596d06cf6bf7347e2cd1336a9c4d0c
Eloah Baracho <eloahbarachodossantos@gmail.com>
2023-06-30 08:10:02 -0300

    Add files via upload

---
+++ swift02.playground/Contents.swift
@@ -0,0 +1,7 @@
+let notaJoao: Float = 8.3
+let notaMaria: Float = 6.1
+let notaAmanda: Float = 8.6
+let notaMedia: Float = (notaJoao + notaMaria + notaAmanda) / 3
+let notaMediaInteira = Int(notaMedia)
+print("A média desses três alunos é: \(notaMediaInteira)")
+
---
+++ swift03.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+var pontuacao1: Int = 10
+var pontuacao2: Int = 5
+
+pontuacao1 > pontuacao2
+pontuacao2 <= pontuacao1
+pontuacao1 == pontuacao2
+pontuacao1 != pontuacao2
+
+var usuario1Venceu = pontuacao1 > pontuacao2
+var usuario2Venceu = pontuacao2 > pontuacao1
+
+var idade = 20
+var adulto: Bool = idade >= 18
+var possuiCNHvalida: Bool = true
+
+var aptoDirigir: Bool = adulto && possuiCNHvalida
+!adulto
